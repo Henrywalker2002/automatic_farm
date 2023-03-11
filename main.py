49958647ec318fd5a83b05b22d1ace04e6b91fb1
Henrywalker2002 <46373762+Henrywalker2002@users.noreply.github.com>
2023-03-11 22:11:20 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from model import *
 import re
 import uvicorn
 from datetime import datetime
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
@@ -172,6 +173,19 @@ async def addDetect(detect: Detection):
 @app.get("/detection")
 async def getDetect():
     return AF.getDetection()
+
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
